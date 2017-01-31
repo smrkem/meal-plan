0152d43d2d7a428195380674ffd540df32aa6d3d
Matt Smrke <matthew.smrke@empire.ca>
2017-01-30 21:56:11 -0500

    adds user to recipes and new views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,38 @@
 class RecipesController < ApplicationController
+    before_action :require_login
+
     def index
-        @recipes = Recipe.all
+        @recipes = current_user.recipes
     end
 
     def show
-        @recipe = Recipe.find(params[:id])
+        @recipe = current_user.recipes.find(params[:id])
+    end
+
+    def new
+        @recipe = current_user.recipes.build
+    end
+
+    def create
+        @recipe = current_user.recipes.build(recipe_params)
+
+        if @recipe.save
+            redirect_to recipe_path(@recipe), notice: "Recipe Created!"
+        else
+            @errors = @recipe.errors.full_messages
+            render :new
+        end
     end
 
     def destroy
-        recipe = Recipe.find(params[:id])
+        recipe = current_user.recipes.find(params[:id])
         recipe.destroy
         redirect_to recipes_path, notice: "Deleted recipe: ${recipe.name}"
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description)
     end
 end
